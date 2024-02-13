44c1bdc3417c34273887473ba5070b2f307e0f36
kmekarski <72306134+kmekarski@users.noreply.github.com>
2024-02-13 12:38:32 +0100

    Initial Commit

---
+++ WhoseLine/WhoseLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoseLineApp.swift
+//  WhoseLine
+//
+//  Created by Klaudiusz Mękarski on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhoseLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
